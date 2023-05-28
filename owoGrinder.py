e7fc07c33f55bebc94738e6d34ec7638d532bfec
NotSuperSaiyan <107041625+NotSuperSaiyan@users.noreply.github.com>
2023-05-28 17:42:14 +0530

    Version 1.1 Added Antiban and Safety
    Added Antiban and safety features and fixed minor bugs

--- owoGrinder.py
+++ owoGrinder.py
@@ -1,26 +1,55 @@
 import pyautogui
 import time
+import random
 
-time.sleep(3)
+time.sleep(5)
 
-for i in range(100):
+for i in range(50):
 
-    pyautogui.write("owo hunt")
-    pyautogui.press('enter')
+    for i in range(10):
 
-    time.sleep(2)
+        pyautogui.write("owo hunt")
+        pyautogui.press('enter')
 
-    pyautogui.write("owo sell all")
-    pyautogui.press('enter')
+        time.sleep(random.randint(3,7))
 
-    time.sleep(2)
+        pyautogui.write("owo sell all")
+        pyautogui.press('enter')
 
-    pyautogui.write("owo battle")
-    pyautogui.press('enter')
+        time.sleep(random.randint(3,7))
 
-    time.sleep(2)
-    
-    pyautogui.write("owo flip 100")
-    pyautogui.press('enter')
+        pyautogui.write("owo battle")
+        pyautogui.press('enter')
 
-    time.sleep(8)
+        time.sleep(random.randint(3,7))
+        
+        pyautogui.write("owo flip 150")
+        pyautogui.press('enter')
+
+        time.sleep(7)
+
+    time.sleep(30)
+
+    for i in range(10):
+
+            pyautogui.write("owo hunt")
+            pyautogui.press('enter')
+
+            time.sleep(random.randint(3,7))
+
+            pyautogui.write("owo battle")
+            pyautogui.press('enter')
+
+            time.sleep(random.randint(3,7))
+
+            pyautogui.write("owo sell all")
+            pyautogui.press('enter')
+
+            time.sleep(random.randint(3,7))
+            
+            pyautogui.write("owo flip 165")
+            pyautogui.press('enter')
+
+            time.sleep(7)
+
+    time.sleep(30)
