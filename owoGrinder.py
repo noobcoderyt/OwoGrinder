a3e2f03db8796d375b8d00339ea99215ac90417e
NotSuperSaiyan <107041625+NotSuperSaiyan@users.noreply.github.com>
2023-05-28 16:10:05 +0530

    Verison 1.0
    Owo AFK grinder

---
+++ owoGrinder.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+
+time.sleep(3)
+
+for i in range(100):
+
+    pyautogui.write("owo hunt")
+    pyautogui.press('enter')
+
+    time.sleep(2)
+
+    pyautogui.write("owo sell all")
+    pyautogui.press('enter')
+
+    time.sleep(2)
+
+    pyautogui.write("owo battle")
+    pyautogui.press('enter')
+
+    time.sleep(2)
+    
+    pyautogui.write("owo flip 100")
+    pyautogui.press('enter')
+
+    time.sleep(8)
